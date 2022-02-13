c498b0b50e544c49d4be6e3e9bac16418918fa87
Yetzin <mermin@protonmail.ch>
2022-02-12 23:35:48 -0600

    pantalla inicio

--- src/pages/Inicio.vue
+++ src/pages/Inicio.vue
@@ -1,14 +1,28 @@
 <template>
-    <div class="relative flex items-center justify-center w-full h-full bg-gray-900 select-none">
-        <div class="flex flex-col p-4 text-white border border-white rounded">
-            <div class="border-b border-gray-600">
-                <span>Seleccione el tamaño de las celdas</span>
+    <div class="relative flex flex-col items-center justify-center w-full h-full bg-gray-900 select-none">
+        <div class="flex flex-col w-full max-w-lg text-white border border-white rounded-lg">
+            <div class="p-4 border-b border-gray-600">
+                <span>Seleccione la configuración deseada:</span>
             </div>
-            <div class="flex justify-start p-4 pb-0">
-                <span>txt</span>
-                <input class="h-10 text-center bg-gray-900 border border-white rounded" type="number" :value="tam">
+            <div class="flex pb-0">
+                <div class="flex flex-col w-2/5 p-4 border-r border-gray-600">
+                    <span>Tamaño de las celdas:</span>
+                    <input class="w-full h-10 text-center bg-gray-900 border border-white rounded" type="number" min="1" v-model="tam">
+                </div>
+                <div class="flex w-3/5 p-4">
+                    <div class="flex flex-col w-1/2">
+                        <span>Celdas en X:</span>
+                        <input class="w-full h-10 text-center bg-gray-900 border border-white rounded" type="number" v-model="celdas_x">
+                    </div>
+                    <div class="w-4"></div>
+                    <div class="flex flex-col w-1/2">
+                        <span>Celdas en Y:</span>
+                        <input class="w-full h-10 text-center bg-gray-900 border border-white rounded" type="number" v-model="celdas_y">
+                    </div>
+                </div>
             </div>
         </div>
+        <button @click="setConfig" class="px-4 py-2 mt-8 font-bold text-white border border-white rounded hover:bg-gray-400 hover:text-blue-900">Comenzar</button>
     </div>
 </template>
 
@@ -17,8 +31,17 @@
         name: 'Inicio',
         data() {
             return {
-                tam: 4
+                tam: localStorage.getItem('tam')? parseInt(localStorage.getItem('tam')) : 4,
+                celdas_x: localStorage.getItem('celdas_x')? parseInt(localStorage.getItem('celdas_x')) : 20,
+                celdas_y: localStorage.getItem('celdas_y')? parseInt(localStorage.getItem('celdas_y')) : 20,
             }
-        }
+        },
+        methods: {
+            setConfig() {
+                localStorage.setItem('tam', this.tam)
+                localStorage.setItem('celdas_x', this.tam)
+                localStorage.setItem('celdas_y', this.tam)
+            }
+        },
     }
 </script>
